81e53c9433d36986d6532e330742f0402da74d15
Damiano <damiano.enerli@mail.polimi.it>
2018-02-13 02:07:04 +0100

    blocks generation and rendering

--- jabbascript/hello.js
+++ jabbascript/hello.js
@@ -1,3 +1,4 @@
+const PIXEL_PROBABILITY = 0.6;
 // field constants:
 const MIN_ROW = 6;
 const MIN_COL = 8;
@@ -7,6 +8,14 @@ const MAX_COL_B = 3;
 // boolean matrices for field and next
 var field_mat = [];
 var next_mat = [];
+
+const t_clock = '↻';
+const t_anti  = '↺';
+const s_right = '→';
+const s_left  = '←';
+const s_down  = '↓';
+
+var playing = false;
 
 // init field
 for(var i = 0; i < MIN_ROW; i++)
@@ -32,93 +41,166 @@ for(var i = 0; i < MAX_ROW_B; i++)
 
 var field_str = '';
 var name = 'damn';
-var score = '200';
-var blocks_counter = '23';
+var score = '0';
+var blocks_counter = '0';
 
 /**
- * toStringField Il metodo per stampare il campo di gioco. Non riceve nessun parametro
- * perchè il campo di gioco è globale possiamo accedere in qualunque
- * momento.
+ * toStringField method to build the field as a string starting from the field
+ *               as a matrix. 
  */
-function toStringField() {
-
-  field_str += '  ┌─';
-  for (var c = 0; c < field_mat[0].length; c++) {
-    field_str += '──';
-  }
+function toStringField()
+{
+  field_str = '  ┌─';
+  for (var c = 0; c < field_mat[0].length; c++)
+  { field_str += '──'; }
   field_str += '─┐  \n';
   field_str += ' *│┌';
-  for (var c = 0; c < field_mat[0].length; c++) {
-    field_str += '──';
-  }
+  for (var c = 0; c < field_mat[0].length; c++)
+  { field_str += '──'; }
   field_str += '┐│* ';
-  field_str += '\tPlayer\t\t' + name + '\n';
+  field_str += '    Player    ' + ('         ' + name).slice(-9) + '\n';
 
   field_str += ' *││';
-  for (var c = 0; c < field_mat[0].length; c++) {
+  for (var c = 0; c < field_mat[0].length; c++)
+  {
     field_str += (c == field_mat[0].length / 2 - 4) ? 'D4MN T E T R I S' : '  ';
-    if (c == field_mat[0].length / 2 - 4) {
-      c += 7;
-    }
+    if (c == field_mat[0].length / 2 - 4)
+    { c += 7; }
   }
   field_str += '││* \n'; //◣◢ maybe this in place of **
 
   field_str += ' *│├';
-  for (var c = 0; c < field_mat[0].length; c++) {
-    field_str += '──';
-  }
+  for (var c = 0; c < field_mat[0].length; c++)
+  { field_str += '──'; }
   field_str += '┤│* ';
-  field_str += '\tScore\t\t' + score + '\n';
+  field_str += '    Score         ' + ('     ' + score).slice(-5) + '\n';
 
   var printingNext = false;
   var counterRowNext = 0;
-  for (var r = 0; r < field_mat.length; r++) { //CIAO DAMI
+  for (var r = 0; r < field_mat.length; r++)
+  { //CIAO DAMI
     field_str += ' *││';
-    for (var c = 0; c < field_mat[r].length; c++) {
-      field_str += field_mat[r][c] ? '██' : ' .';
-    }
+    for (var c = 0; c < field_mat[r].length; c++)
+    { field_str += field_mat[r][c] ? '██' : ' .'; }
     field_str += '││* ';
-    if (r == 3) {
-      printingNext = true;
-    }
-    if (r == 1) {
-      field_str += '\tBlocks\t\t' + blocks_counter + '\n';
-    } else if (printingNext) {
-      if (counterRowNext == (Math.floor(next_mat[0].length / 2 - 0.1))) {
-        field_str += '\tNext\t\t';
-      } else {
-        field_str += '\t\t\t';
-      }
+    if (r == 3) 
+    { printingNext = true; }
+    if (r == 1)
+    { field_str += '    Blocks        ' + ('     ' + blocks_counter).slice(-5) + '\n'; }
+    else if (printingNext)
+    {
+      if (counterRowNext == (Math.floor(next_mat[0].length / 2 - 0.1)))
+      { field_str += '    Next         '; }
+      else
+      { field_str += '                 '; }
       printBlockRow(counterRowNext, next_mat);
       field_str += '\n';
       counterRowNext++;
-      if (counterRowNext >= MAX_ROW_B) {
-        printingNext = false;
-      }
-    } else if (r == MAX_ROW_B + 4) {
+      if (counterRowNext >= MAX_ROW_B)
+      { printingNext = false; }
+    } else if (r == MAX_ROW_B + 4)
+    {
       /* qui si può aggiungere qualcosa all'interfaccia. */
       // in tal caso bisognerebbe incrementare il numero minimo di righe.
       field_str += '\t';
-    } else {
-      field_str += '\n';
     }
+    else
+    { field_str += '\n'; }
   }
   field_str += '──┴┴';
-  for (var c = 0; c < field_mat[1].length; c++) {
-    field_str += '──';
-  }
+  for (var c = 0; c < field_mat[1].length; c++)
+  { field_str += '──'; }
   field_str += '┴┴──\n////////';
-  for (var c = 0; c < field_mat[1].length; c++) {
-    field_str += '//';
-  }
+  for (var c = 0; c < field_mat[1].length; c++)
+  { field_str += '//'; }
   field_str += '\n';
+  console.log(field_str);
 }
 
-
-function printBlockRow(row, block) {
-  for (var c = 0; c < block[0].length; c++) {
-    field_str += block[row][c] ? '██' : ' .';
+function genBlock()
+{
+  var block = [];
+  for(var i = 0; i < MAX_ROW_B; i++)
+  {
+    var row = new Array();
+    for(var j = 0; j < MAX_COL_B; j++)
+    {
+      row.push(false);
+    }
+    block.push(row);
   }
+  console.log(block);
+  var first = Math.floor(Math.random() * MAX_ROW_B * MAX_COL_B);
+  console.log(Math.random());
+  console.log(first);
+  var counter = 0;
+  for (var i = 0; i < MAX_ROW_B; i++)
+  {
+    for (var j = 0; j < MAX_COL_B; j++)
+    {
+      if (first == counter)
+      { block[i][j] = true; }
+      counter++;
+    }
+  }
+  for (var i = 0; i < MAX_ROW_B; i++)
+  {
+    for (var j = 0; j < MAX_COL_B; j++)
+    {
+      if (adiacency(i, j, block))
+      {
+        if (Math.random() < PIXEL_PROBABILITY)
+        { block[i][j] = true; }
+      }
+    }
+  }
+  return block;
+  console.log(block);
+}
+
+function adiacency(row, col, block) {
+  switch (row) {
+    case 0:
+      if (block[row + 1][col])
+      { return true; }
+      break;
+    case MAX_ROW_B - 1:
+      if (block[row - 1][col])
+      { return true; }
+      break;
+    default:
+      if (block[row + 1][col] || block[row - 1][col])
+      { return true; }
+  }
+  switch (col) {
+    case 0:
+      if (block[row][col + 1])
+      { return true; }
+      break;
+    case MAX_COL_B - 1:
+      if (block[row][col - 1])
+      { return true; }
+      break;
+    default:
+      if (block[row][col + 1] || block[row][col - 1])
+      { return true; }
+  }
+  return false;
+}
+
+var commands = '' +
+    '┌───┬───┐\n' +
+    '│ ' + t_anti + ' │ ' + t_clock + ' │\n' +
+    '└───┴───┘\n' +
+    '   ┌───┬───┬───┐\n' +
+    '   │ ' + s_left + ' │ ' + s_down + ' │ ' + s_right + ' │\n' +
+    '   └───┴───┴───┘\n';
+
+
+function printBlockRow(row, block)
+{
+  for (var c = 0; c < block[0].length; c++)
+  { field_str += block[row][c] ? '██' : ' .'; }
 }
 
 toStringField();
@@ -127,30 +209,39 @@ var app = new Vue({
   el: '#tetris-app',
 
   data: {
-    intro: '██████  ██████  ██████  ██████  ██  ██████\n' +
-           '  ██    ████      ██    ██  ██  ██  ██    \n' +
-           '  ██    ██        ██    ████    ██      ██\n' +
-           '  ██    ██████    ██    ██  ██  ██  ██████\n' +
-           '         ascii tetris game by damn1       \n',
-    field_str: field_str,
-    next_str: '',
-
+    intro: '' +
+    '██████  ██████  ██████  ██████  ██  ██████\n' +
+    '  ██    ████      ██    ██  ██  ██  ██    \n' +
+    '  ██    ██        ██    ████    ██      ██\n' +
+    '  ██    ██████    ██    ██  ██  ██  ██████\n' +
+    '            ascii tetris by damn1         \n',
+    start: '' +
+    '┌─────────────┐\n' +
+    '     START     \n' +
+    '└─────────────┘\n',
+    field_str_vue: field_str,
+    commands: commands,
     // pieces random geneation:
-    PIXEL_PROBABILITY: .6,
     score: 0,
     blocks_counter: 0,
     name: ''
   },
 
   methods: {
-    getRows: function() {
-      console.log(field_mat);
-      //      if (e.keyCode === 13) {
-      //      alert('Enter was pressed');
-      //    } else if (e.keyCode === 50) {
-      //    alert('@ was pressed');
-      //    }      
-      //    this.log += e.key;
+    keymonitor: function(event) {
+      console.log(event.key);
+      if(event.key == "Enter")
+      {
+        console.log("enter key was pressed!");
+      }
+    },
+    startMatch: function() {
+      // `this` inside methods points to the Vue instance
+      playing = true;
+      console.log(playing);
+      next_mat = genBlock();
+      toStringField();
+      this.field_str_vue = field_str;
     }
   }
 });
